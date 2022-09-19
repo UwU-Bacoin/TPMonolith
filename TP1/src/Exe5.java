8a150fb15ee53ff52f9533df010f0cefc26852dd
Jaggernaute <lucas.videcoq1@gmail.com>
2022-09-20 01:12:11 +0200

    :sparkles: Add solution for Lab5

--- TP1/src/Exe5.java
+++ TP1/src/Exe5.java
@@ -1,9 +1,25 @@
 package tp1;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Exe5 {
 	//Affichage dans l'ordre croissant
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] tab = new int[3];
+		System.out.println("Entrez 3 nombres");
+		tab[0] = sc.nextInt();
+		tab[1] = sc.nextInt();
+		tab[2] = sc.nextInt();
+
+		Arrays.sort(tab);
+
+		System.out.println("Les nombres dans l'ordre croissant sont :");
+		for (int i : tab) {
+			System.out.println(i);
+		}
 
 	}
 }
