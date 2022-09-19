42ff11d8aae4e91ee70bf920d04f1b8b9f571c98
Jaggernaute <lucas.videcoq1@gmail.com>
2022-09-20 00:46:25 +0200

    :sparkles: Add solution for Lab2

--- TP1/src/Exe2.java
+++ TP1/src/Exe2.java
@@ -6,10 +6,24 @@ public class Exe2 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		int a, b, temp;
 
+		System.out.println("Entrez un nombre entier :");
+		a = sc.nextInt();
+		System.out.println("Entrez un autre nombre entier :");
+		b = sc.nextInt();
+		System.out.println("a = " + a + "\net b = " + b);
 
+		temp = a;
+		a = b;
+		b = temp;
+		System.out.println("a = " + a + "\net b = " + b);
 
+		System.out.println("a*2 = " + a*2 + "\nb/2 = " + b/2);
 
+		System.out.println("a/b = " + a/b + "\nb%a = " + b%a);
+
+		System.out.println(a>b ? "a est plus grand que b" : "b est plus grand que a");
 
 		sc.close();
 	}
