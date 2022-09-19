dea82b13571d0600e7554564208fe5dfe3d05c7e
Jaggernaute <lucas.videcoq1@gmail.com>
2022-09-20 00:55:45 +0200

    :sparkles: Add solution for Lab4

--- TP1/src/Exe4.java
+++ TP1/src/Exe4.java
@@ -1,9 +1,23 @@
 package tp1;
 
+import java.util.Scanner;
+
 public class Exe4 {
 	//Parité d'une somme
 
 	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int a, b, c;
+		System.out.println("Entrez 3 entiers");
+		a = s.nextInt();
+		b = s.nextInt();
+		c = s.nextInt();
 
+		if((a+b+c)%2 == 0) {
+			System.out.println("La somme est paire");
+		}
+		else {
+			System.out.println("La somme est impaire");
+		}
 	}
 }
