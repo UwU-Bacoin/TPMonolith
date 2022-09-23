0db5ea891b40d246d0a55325e280b9f1368058d1
Jaggernaute <lucas.videcoq1@gmail.com>
2022-09-23 23:21:02 +0200

    :sparkles: Add tests to lab1

--- TP1/src/Exe1.java
+++ TP1/src/Exe1.java
@@ -1,9 +1,17 @@
 package tp1;
 
-public class Exe1 {
-	//Hello World !
+import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
+public class Exe1 {
+	public static String s = "Hello World";
 	public static void main(String[] args) {
-		System.out.println("Hello World !");
+		System.out.println(s);
+	}
+
+	@Test
+	public void test() {
+		assertEquals("Hello World", s);
 	}
 }
