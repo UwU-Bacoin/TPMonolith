c4c7e70eb1f620a3b78dadeb1be739a61fd3aa9e
Jaggernaute <lucas.videcoq1@gmail.com>
2022-09-25 02:28:54 +0200

    :sparkles: Add tests to lab2

--- TP1/src/Exe2.java
+++ TP1/src/Exe2.java
@@ -1,30 +1,124 @@
 package tp1;
+
+import org.junit.Rule;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.Scanner;
 
 public class Exe2 {
-	//Sasie et opérations de base
+	int a,b;
 
 	public static void main(String[] args) {
+
+		int[] input = input();
+
+		swap(input[0], input[1]);
+		randoMaths(input[0], input[1]);
+		isGreater(input[0], input[1]);
+	}
+
+	public static int[] input() {
 		Scanner sc = new Scanner(System.in);
-		int a, b, temp;
+		System.out.println("Entrez un nombre");
+		int a = sc.nextInt();
+		System.out.println("Entrez un autre nombre");
+		int b = sc.nextInt();
+		return new int[]{a, b};
+	}
 
-		System.out.println("Entrez un nombre entier :");
-		a = sc.nextInt();
-		System.out.println("Entrez un autre nombre entier :");
-		b = sc.nextInt();
-		System.out.println("a = " + a + "\net b = " + b);
-
-		temp = a;
+	static int[] swap(int a, int b) {
+		int[] tab = new int[2];
+		int temp = a;
 		a = b;
 		b = temp;
-		System.out.println("a = " + a + "\net b = " + b);
+		tab[0] = a;
+		tab[1] = b;
+		System.out.println("\n========== swap() ==========");
+		System.out.println("a <=> b");
+		System.out.println("> a = " + a + "\n> b = " + b);
+		return tab;
+	}
 
-		System.out.println("a*2 = " + a*2 + "\nb/2 = " + b/2);
+	/**
+	 * @description: Fonction that double the first parameter and divides the second parameter by two
+	 *
+	 * @param a int
+	 * @param b int
+	 * @return int[] tab
+	 */
+	static int[] randoMaths(int a, int b) {
+		int[] tab = new int[2];
+		tab[0] = a*2;
+		tab[1] = b/2;
+		System.out.println("\n========== randoMaths() ==========");
+		System.out.println("> a*2 = " + a*2 + "\n> b/2 = " + b/2);
+		return tab;
+	}
 
-		System.out.println("a/b = " + a/b + "\nb%a = " + b%a);
+	public static boolean isGreater(int a, int b) {
+		if(a > b) {
+			System.out.println("\n========== isGreater() ==========");
+			System.out.println("a est plus grand que b");
+			return true;
+		} else if (a < b) {
+			System.out.println("\n========== isGreater() ==========");
+			System.out.println("b est plus grand que a");
+			return false;
+		} else {
+			System.out.println("\n========== isGreater() ==========");
+			System.out.println("a et b sont égaux");
+			return false;
+		}
+	}
 
-		System.out.println(a>b ? "a est plus grand que b" : "b est plus grand que a");
+	/*
+	 * Unit test for the Exe2 class.
+	 */
 
-		sc.close();
+	/**
+	 * Tests the swap() method.
+	 */
+	@Test
+	public void testSwap() {
+		int[] tab = swap(1, 2);
+		Assertions.assertEquals(2, tab[0]);
+		Assertions.assertEquals(1, tab[1]);
+	}
+
+	@Test
+	public void testInput() {
+
+		String input1 = "5\n10";
+		InputStream in1 = new ByteArrayInputStream(input1.getBytes());
+		System.setIn(in1);
+		Assertions.assertArrayEquals(new int[]{5, 10} , input());
+
+		String input2 = "a\nl";
+		InputStream in2 = new ByteArrayInputStream(input2.getBytes());
+		System.setIn(in2);
+	}
+
+	/**
+	 * Tests the randoMaths() method.
+	 */
+	@Test
+	public void testRandomMaths() {
+		Assertions.assertArrayEquals(new int[]{10, 15}, randoMaths(5, 30));
+		Assertions.assertArrayEquals(new int[]{0, 0}, randoMaths(0, 0));
+		Assertions.assertArrayEquals(new int[]{-10, -15}, randoMaths(-5, -30));
+	}
+
+	/**
+	 * Tests the isGreater() method.
+	 */
+	@Test
+	public void testIsGreater() {
+		Assertions.assertTrue(isGreater(10, 5));
+		Assertions.assertFalse(isGreater(5, 10));
+		Assertions.assertFalse(isGreater(0,0));
+		Assertions.assertTrue(isGreater(-5, -10));
 	}
 }
