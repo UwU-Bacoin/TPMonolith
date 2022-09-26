668863a3465f63a0a0d14bf43a5dc2d555a48343
Jaggernaute <lucas.videcoq1@gmail.com>
2022-09-26 04:54:08 +0200

    :test_tube: Added Tests to lab3

--- TP1/src/Exe3.java
+++ TP1/src/Exe3.java
@@ -1,5 +1,10 @@
 package tp1;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import java.util.Scanner;
+
+import static org.junit.Assert.assertTrue;
 
 public class Exe3 {
 	//Année bissextile
@@ -9,12 +14,32 @@ public class Exe3 {
 		System.out.println("Entrez une année :");
 		int year = sc.nextInt();
 
-		if (year%4==0 && year%100!=0 || year%400==0) {
-			System.out.println("L'année est bissextile");
+		if (isBissextile(year)) {
+			System.out.println("L'année " + year + " est bissextile");
+		} else {
+			System.out.println("L'année " + year + " n'est pas bissextile");
+		}
+
+		sc.close();
+	}
+
+	public static boolean isBissextile(int year) {
+		if(year % 4 == 0) {
+			if(year % 100 == 0) {
+				return year % 400 == 0;
+			}
+			else {
+				return true;
+			}
 		}
 		else {
-			System.out.println("L'année n'est pas bissextile");
+			return false;
 		}
-		sc.close();
+	}
+
+	@Test
+	public void test() {
+		Assertions.assertTrue(isBissextile(2000));
+		Assertions.assertFalse(isBissextile(2001));
 	}
 }
