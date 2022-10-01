99cc0977258cd2c5c675933ab153f7a0cf5e3b66
jaggernaute <lucas.videcoq1@gmail.com>
2022-10-01 02:52:33 +0200

    :sparkles: Add solution to lab2 x2

--- TP2/src/Exe2.java
+++ TP2/src/Exe2.java
@@ -4,7 +4,14 @@ public class Exe2 {
 	//Conversion degrés Fahrenheit vers Celsius
 	
 	public static void main(String[] args) {
+		float f = 250;
+		float c;
 
+		while (f >= -20) {
+			c = (f - 32) * 5 / 9;
+			System.out.println(f + "°F =====> " + c + "°C");
+			f -= 10;
+		}
 	}
 
 }
