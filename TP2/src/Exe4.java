053ced7e51d4c40dcd55bca11b74febd167c2850
jaggernaute <lucas.videcoq1@gmail.com>
2022-10-01 02:58:45 +0200

    :sparkles: Add solution to lab2 ex4

--- TP2/src/Exe4.java
+++ TP2/src/Exe4.java
@@ -13,6 +13,22 @@ public class Exe4 {
 	}
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int nombre = entierAleatoire(1, 100);
+		int nbEssais = 0;
+		int nb = 0;
+		while (nb != nombre) {
+			System.out.println("Entrez un nombre :");
+			nb = sc.nextInt();
+			nbEssais++;
+			if (nb < nombre) {
+				System.out.println("C'est plus");
+			} else if (nb > nombre) {
+				System.out.println("C'est moins");
+			}
+		}
+		System.out.println("Bravo, vous avez trouvé en " + nbEssais + " essais");
+		sc.close();
 
 	}
 }
