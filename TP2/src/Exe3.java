265d17f8c685edd2292cf786cc050c9db9f7db11
jaggernaute <lucas.videcoq1@gmail.com>
2022-10-01 02:55:41 +0200

    :sparkles: Add solution to lab2 ex3

--- TP2/src/Exe3.java
+++ TP2/src/Exe3.java
@@ -5,7 +5,25 @@ public class Exe3 {
 	// Somme de 1 à n
 	
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Entrez un nombre :");
+		int n = sc.nextInt();
+		if (n < 0) {
+			System.out.println("Le nombre doit être positif");
+		} else {
+			StringBuilder sb = new StringBuilder();
+			int sum = 0;
+			for (int i = 1; i <= n; i++) {
+				sum += i;
+				sb.append(i);
+				if (i != n) {
+					sb.append(" + ");
+				}
+			}
+			sb.append(" = ");
+			sb.append(sum);
+			System.out.println(sb.toString());
+		}
 	}
 
 }
